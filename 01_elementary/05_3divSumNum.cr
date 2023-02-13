cc8124c0bc0f08eb04163e187fe8d9fb07a74d20
manemobiili <jouni@nasi.mshome.net>
2023-02-13 13:44:58 +0000

    More concise solution using .sum

--- 01_elementary/05_3divSumNum.cr
+++ 01_elementary/05_3divSumNum.cr
@@ -1,20 +1,10 @@
-p "Give me a number and i'll sum from 1 to that number,"
-p "but this time it has to be divisable by 3 or 5."
+p "Give me a number and i'll sum from 1 to that number, but it has to be divisible by 3 or 5."
 
 number = gets.not_nil!.chomp.to_i
 
-unless (number % 3 & 5).zero?
-  p "Sorry, number has to divisable by 3 or 5."
+if (number % 3 & 5).zero?
+  numWalk = (1..number).to_a
+  p numWalk.sum
+else
+  p "Sorry, your number is not divisible by 3 or 5."
 end
-
-def sum(n)
-    numbers = (1..n).to_a
-      sum = 0
-      numbers.each do |n|
-      sum += n
-    end 
-  p sum
-end
-
-sum(number)
-
