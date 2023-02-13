51f672bf9d57e683382a2eee53c145cc0da4d4b9
manemobiili <jouni@nasi.mshome.net>
2023-02-13 13:51:54 +0000

    More concise solution using .sum

--- 01_elementary/04_sumNum.cr
+++ 01_elementary/04_sumNum.cr
@@ -2,13 +2,5 @@ p "Give me a number and i'll sum from 1 to that number"
 
 number = gets.not_nil!.chomp.to_i
 
-def sum(n)
-    numbers = (1..n).to_a
-      sum = 0
-      numbers.each do |n|
-      sum += n
-    end 
-  p sum
-end
-
-sum(number)
+numWalk = (1..number).to_a
+p numWalk.sum
