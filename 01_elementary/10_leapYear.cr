6ebc7df1844eecebd54a889a0c742c667d91ad82
manemobiili <jouni@nasi.mshome.net>
2023-02-17 04:00:13 +0000

    Less variables (startYear)

--- 01_elementary/10_leapYear.cr
+++ 01_elementary/10_leapYear.cr
@@ -1,7 +1,2 @@
 time = Time.utc
-current = time.year
-
-startYear = current - (current % 4)
-startYear += 4
-
-20.times { |i| p startYear + i * 4 }
+20.times { |i| puts (time.year + 4 - (time.year % 4)) + i * 4 }
