54d155b63e41f8666c121d6451a1ee1648778f49
manemobiili <jouni@nasi.mshome.net>
2023-02-17 18:18:22 +0000

    Explain the thought process. Give an example.

--- 01_elementary/10_leapYear.cr
+++ 01_elementary/10_leapYear.cr
@@ -1,2 +1,5 @@
-# Because were not dealing with floating point numbers, 2023 + 4 - 2023 % 4 gives us the next leap year
+# % is the modulus. For example:
+# 2023 + 4 = 2027
+# 2023 % 4 = 3
+# 2027 - 3 = 2024
 20.times { |i| puts (Time.utc.year + 4 - (Time.utc.year % 4)) + i * 4 }
