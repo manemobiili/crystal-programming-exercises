10df412a66ff5447496ab24d38bac376a560220d
manemobiili <jouni@nasi.mshome.net>
2023-02-17 18:08:13 +0000

    Output divisible numbers, modify factorial to use ternary operator

---
+++ 01_elementary/06_mathProduct.cr
@@ -0,0 +1,44 @@
+p "Please choose a number"
+number = gets.not_nil!.chomp.to_i
+
+  # Todo: calculate a set of products of number
+  p "Type d to calculate the dividing factors of #{number}"
+
+  # Factorials bigger than 12 cause an overflow
+  number <= 12 ? p "Type f to calculate the #{number}'s factorial" : p "Unfortunately 12 is the largest factorial i am able to calculate."
+
+  p "Type s to calculate the sum from 1 to #{number}"
+
+choice = gets.not_nil!.downcase
+
+case choice
+      when "d"
+        factors = [] of Int32
+        (1..number).each do |n|
+          factors << n if number % n == 0
+        end
+
+        p "The factors of #{number} are: #{factors.join(", ")}"
+      exit
+
+      when "f"
+        fact = 1
+        i = 1
+
+        while (i <= number)
+          fact = fact*i
+          i += 1
+        end
+ 
+        p fact
+      exit
+
+      when "s"
+        numWalk = (1..number).to_a
+        p numWalk.sum
+      exit
+
+      else
+        p "try again"
+      exit
+end
